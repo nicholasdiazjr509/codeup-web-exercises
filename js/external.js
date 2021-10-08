f529cb6e50551e5778ff19a36cafdd5c82ebedd0
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-08 12:01:45 -0500

    s

---
+++ js/external.js
@@ -0,0 +1,42 @@
+"use strict"
+
+console.log( "Hello from external JavaScript");
+
+alert("Red alert dude!!");
+
+var userInput = prompt("What is your favorite color?");
+alert("That's my favorite color too!!");
+
+// var jeff = "Jeff";
+// console.log(jeff);
+//
+// console.log("Hello, im in a separate file!");
+
+
+
+// var returnOfConfirm = confirm("Got any plans for the darn weekend?");
+// console.log(returnOfConfirm);
+//
+// var favoriteNuMetalBand = prompt (message) ("What's your favorite nu-metal band?");
+// console.log("Ha ha, you like " + favoriteNuMetalBand + "!");
+//
+// var leastFavoriteSoda = prompt(message)("what is your least favorite soda?");
+// var favoriteSoda = prompt(message)("what is your favorite soda")
+
+
+
+ //(littleMermaid + brotherBear + hercules) *3;
+
+//var littleMermaid = 3 * 3;
+//var brotherBear = 5 * 3;
+//var hercules = 1 * 3;
+
+
+function priceForMovies(x, y, z){
+    return littleMermaid + brotherBear + hercules;
+}
+
+console.log(priceForMovies(littleMermaid, brotherBear, hercules));
+
+
+ var days = ["littleMermaid" + "brotherBear" + "hercules"];
