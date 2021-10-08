7a7a481c34e62a15bbdbed0e62ba7db4e6231043
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-08 15:36:34 -0500

    commit

--- js/external.js
+++ js/external.js
@@ -38,13 +38,22 @@ alert("That's my favorite color too!!");
 
 //(littleMermaid + brotherBear + hercules) *3;
 
-var littleMermaid ;
-var brotherBear ;
-var hercules ;
+var littleMermaid = prompt("How many days did you rent The Little Mermaid?");
+var brotherBear = prompt("How many days did you rent Brother Bear?");
+var hercules = prompt("How many days did you rent hercules?");
+var costOfMovies = prompt("How much per day?")
+//Could have used Parse
 
+var littleMermaidCost = Number(littleMermaid) * Number(costOfMovies);
+var brotherBearCost = Number(brotherBear) * Number(costOfMovies);
+var herculesCost = Number(hercules) * Number(costOfMovies);
+var totalRental = littleMermaidCost + brotherBearCost + herculesCost;
+alert (" Your total rental is: " + totalRental);
+
+//
 //var priceForMovies = parseFloat(3.00);
-var movie = prompt("Which movie did you watch?")
-
+//var movie = prompt("Which movie did you watch?")
+//alert(movie);
 
 var days = prompt("Enter days: ");
 alert("Cost of movie rentals " + (days * 3));
@@ -77,61 +86,80 @@ alert(perHour*(google || amazon || facebook));
 
 // A student can be enrolled in a class only if the class is not full and the class
 // schedule does not conflict with her current schedule.
+//
+// var classNotFull = true;
+// var classConflict = true;
+// var classFull = true;
 
-var classNotFull = true;
-var classConflict = true;
-var enrolled = classNotFull && classConflict;
+// var enrolled = classNotFull && classConflict;
+var classNotFull = confirm("Does the classroom have room? ");
+console.log(classNotFull);
 
-var classAt8am = false;
-var classSize = 125;
-var currentClassSize = 108;
-var canRegister = (currentClassSize < classSize) && !classAt8am;
+var scheduleNotConflict = confirm("Do you have availability at 8am")
 
+var enroll = classNotFull && scheduleNotConflict;
+alert("Yes, you can enroll " + enroll);
 
-//     A product offer can be applied only if a person buys more than 2 items,
-//     and the offer has not expired. Premium members do not need to buy a specific
-//     amount of products.
+// var classAt8am = false;
+// var classSize = 25;
+// var currentClassSize = 18;
+// var canRegister = (currentClassSize < classSize) && !classAt8am;
+
+//_____________________________________________________________________//
 
 // var hasPremium = true;
 // var cartSize = 6;
 // var offerValid = true;
 
 // var canHaveDiscount = offerValid && (cartSize > 2 || hasPremium);
-
-
-
-
 // premiumMember = true;
 // offerValid = true;
 // moreThanTwo => 2 && offerValid == true;
 
+//A product offer can be applied only if a person buys more than 2 items,
+// and the offer has not expired.
+// Premium members do not need to buy a specific amount of products.
+
+// var productOffer >2;
+// var offerNotExpired;
+
+var offerNotExpired = confirm("Is the offer not expired");
+console.log(offerNotExpired);
+
+var moreThanTwo = confirm("Bought 2 or more items?")
+console.log(moreThanTwo);
+
+var premiumMember = confirm("Premium member and bought 2 or more items?");
+console.log(premiumMember);
+
+var productOffer = (offerNotExpired && (moreThanTwo || premiumMember));
+
+var discountApplied = alert("Discount may be applied" + productOffer);
+console.log(discountApplied);
+
+
 
 //______________________________________________________________________//
-
-
-
-
-
 // password must be at least 5 characters
 //the password must not include the username
-var username = 'codeup';
-var password = 'notastrongpassword';
-
-var atLeastFivePassword = password.length >= 5;
-var password.length >= 5;
-
-
-//var usernameNotInPassword = password.indexOf(username) === -1;
-// var usernameNotInPassword = ! password.includes(username;
-// the username must be no more than 20 characters
-// var userNameNotaParagraph = username.length <= 20;
-
-// var username.length <= 20;
-// neither the username or password can start or end with whitespace
+// var username = 'codeup';
+// var password = 'notastrongpassword';
+// var atLeastFivePassword = password.length >= 5;
+//
+// var atLeastFivePassword =prompt("Enter password: " );
+//
+//
+// //var usernameNotInPassword = password.indexOf(username) === -1;
+// // var usernameNotInPassword = ! password.includes(username);
+//
+// // the username must be no more than 20 characters
+//  var userNameNotaParagraph = username.length <= 20;
+//  var username.length <= 20;
+//
+// // neither the username or password can start or end with whitespace
 // var whitespace = username.trim() && password.trim();
-
 // var noWhitespace = username.trim() === username && password.trim() === password;
-
+//
 // username.trim();
 // password.trim();
 
