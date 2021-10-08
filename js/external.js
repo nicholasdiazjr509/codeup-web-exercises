c6a2bb034027fde187cd1ca0e46bbc113de718bf
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-08 14:03:18 -0500

    commit

--- js/external.js
+++ js/external.js
@@ -22,21 +22,54 @@ alert("That's my favorite color too!!");
 // var leastFavoriteSoda = prompt(message)("what is your least favorite soda?");
 // var favoriteSoda = prompt(message)("what is your favorite soda")
 
+//     You have rented some movies for your kids: The little mermaid (for 3 days),
+//     Brother Bear (for 5 days, they love it),
+//     and Hercules (1 day, you don't know yet if they're going to like it).
+//     If price for a movie per day is $3, how much will you have to pay?
+// var littleMermaid = 3;
+// var brotherBear = 5;
+// var hercules = 1;
+// (littleMermaid + brotherBear + hercules) *3;
 
 
+// var numberOfDays = prompt ("How many days for movies?");
+// console.log("How many days?" + numberOfDays );
 
-var numberOfDays = prompt ("How many days for movies?");
-console.log("How many days?" + numberOfDays );
- //(littleMermaid + brotherBear + hercules) *3;
 
-//var littleMermaid = 3 * 3;
-//var brotherBear = 5 * 3;
-//var hercules = 1 * 3;
+//(littleMermaid + brotherBear + hercules) *3;
+
+var littleMermaid ;
+var brotherBear ;
+var hercules ;
 
 //var priceForMovies = parseFloat(3.00);
-
-function priceForMovies(x, y, z)
-console.log(priceForMovies(littleMermaid, brotherBear, hercules));
+var movie = prompt("Which movie did you watch?")
 
 
- var days = ["littleMermaid" + "brotherBear" + "hercules"];
+var days = prompt("Enter days: ");
+alert("Cost of movie rentals " + (days * 3));
+
+
+// function priceForMovies(x, y, z);
+// console.log(priceForMovies(littleMermaid, brotherBear, hercules));
+//  var days = ["littleMermaid" + "brotherBear" + "hercules"];
+
+
+//     Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook,
+//     they pay you a different rate per hour.
+//     Google pays $400,
+//     Amazon $380,
+//     and Facebook $350.
+//     How much will you receive in payment for this week?
+//     You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+var google = 400;
+var amazon = 380;
+var facebook = 350;
+
+var company =prompt("Enter company worked at: ")
+alert =(google || amazon || facebook);
+
+
+var perHour = prompt("Enter hours: ");
+alert("Paycheck " + (perHour * (google || amazon || facebook)));
