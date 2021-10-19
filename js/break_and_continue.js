6dba4215a3d07045508699432ec2e65944a62b47
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-19 11:01:27 -0500

    commit

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,20 +1,29 @@
 "use strict";
-( function ())
-{
+( function (){
 
 
 //#2
 // Prompt the user for an odd number between 1 and 50.
 // Use a loop and a break statement to continue prompting the user if they enter invalid input.
+while(true)
+{
+    oddNumber = parseInt(prompt("enter an odd number up to 50: "));//remember it returns a string
 
-    oddNumber = prompt("enter an odd number: ");
-
-    for (var i = 1; i <= 10; i++) {
-        if (oddNumber % 2 == 0) {
-            var evenNumber;
-            evenNumber = prompt("That's even yo!" + " Enter an odd number: ");
-            break;
-            oddNumber = prompt("Enter an odd number please: ");
+    if(oddNumber >= 1 && <= 50 && oddNumber % 2 !== 0)
+    {
+        break;
+    }
+}
+ //instantiate your for loop
+    for (var i = 1; i <= 50; i++) {
+        if (i % 2 == 0) {
+            continue;
+        }
+        if (j === oddNumber) {
+            console.log("we dont need" + oddNumber + " this!");
+            continue;
+        }
+        console.log("This is an odd number " + j);
 
         }
 //#3
@@ -32,6 +41,6 @@
             oddNumber = prompt(oddNumber + " is an odd number")
             console.log(oddNumber + " is an odd number!");
         }
-    }
 
-})()
+
+})();
--- js/while.js
+++ js/while.js
@@ -1,15 +1,37 @@
 "use strict";
 
 // #2. Create a while loop that uses console.log()
+( function (){
 
-//var i = 1;
-while (i < 16) {
-    var j = (i*2);
-    console.log(j)
-    i+=1;
 
-  //  console.log(i.toString().repeat(i));
+
+var multipliedNum = 2;
+while (multipliedNum <= 65536) {
+
+    multipliedNum *= 2;
+    console.log(multipliedNum);
 }
-//   for (  i = 1; i <=9; i++) {
+  //  console.log(i.toString().repeat(i));
+ //   for (  i = 1; i <=9; i++) {
 //         break;
 //     }  ** infinite loop**
+
+//Random number between 50 and 100
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+    console.log("Opening up shop! I've got " + allCones + " cones to sell!");
+
+    do {
+        var conesToBuy = Math.floor(Math.random() * 5) + 1;
+
+        if (conesToBuy <= allCones) {
+            allCones -= conesToBuy;
+            console.log("Customer want " + conesToBuy + " cones. I have " + allCones + " cones left.");
+        } else {
+            console.log("We are out of stock " + conesToBuy + " cones. The store only has " + allCones + " comnes left in store")
+
+        }
+    }
+    while (allCones !== 0)
+            console.log("Close the store.");
+    })();
