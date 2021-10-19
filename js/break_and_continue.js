533f98f2b0e86feee5c2cb3aa996313233629e52
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-18 21:11:27 -0500

    commit

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,9 +4,10 @@
 // Prompt the user for an odd number between 1 and 50.
 // Use a loop and a break statement to continue prompting the user if they enter invalid input.
 
-var oddNumber = prompt("enter an odd number: ");
+     oddNumber = prompt("enter an odd number: ");
 
-while(!oddNumber % 2 == 0){
+    for (var i = 1; i <= 10; i++) {
+     if (oddNumber % 2 == 0){
     var evenNumber;
     evenNumber = prompt("That's even yo!" + " Enter an odd number: ");
       break;
@@ -16,12 +17,17 @@ while(!oddNumber % 2 == 0){
 //#3
 // Use a loop and the continue statement to output all the odd numbers between 1 and 50,
 // except for the number the user entered.
-for (var i = 1; i <= 50; i++){
-    if (i % 2 !==0){
+for (var i = 1; i <= 10; i++) {
+    if ((i % 2) == 0) {
+        continue;
 
-      i =prompt( oddNumber + "Yikes, not that one");
+        console.log(i);
 
-       i = prompt(oddNumber + " is an odd number");
-
+        var oddNumber = prompt(oddNumber + "Yikes, not that one!")
+        console.log(oddNumber+"yikes, not that one!");
     }
-}
+          oddNumber = prompt(oddNumber + " is an odd number")
+            console.log(oddNumber + " is an odd number!");
+    }}
+
+
