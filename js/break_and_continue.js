bb5916a8a001a5c25b6d29de1ba782bce3442b87
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-19 20:05:45 -0500

    commit

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,44 +1,46 @@
-"use strict";
-( function () {
 
+(function () {
+
+"use strict";
 
 //#2
 // Prompt the user for an odd number between 1 and 50.
 // Use a loop and a break statement to continue prompting the user if they enter invalid input.
-while(true)
-{
-    oddNumber = parseInt(prompt("enter an odd number up to 50: "));//remember it returns a string
+var oddNumber;
 
-    if(oddNumber >= 1 && <= 50 && oddNumber % 2 !== 0)
+    while(true)
+{
+    oddNumber = parseInt(prompt("enter an odd number up to 50 "));
+    //remember PROMPT it returns a string
+    if(oddNumber >= 1 && oddNumber <= 50 && oddNumber % 2 !== 0)
     {
         break;
     }
 }
  //instantiate your for loop
-    for (var i = 1; i <= 50; i++) {
-        if (i % 2 == 0) {
-            continue;
-        }
-        if (j === oddNumber) {
-            console.log("we dont need" + oddNumber + " this!");
-            continue;
-        }
-        console.log("This is an odd number " + j);
-
-        }
+ //    for (var i = 1; i <= 50; i++) {
+ //        if (i % 2 == 0) {
+ //            continue;
+ //        }
+ //        if (j === oddNumber) {
+ //            console.log("we dont need" + oddNumber + " this!");
+ //            continue;
+ //        }
+ //        console.log("This is an odd number " + j);
+ //
+ //        }
 //#3
 // Use a loop and the continue statement to output all the odd numbers between 1 and 50,
 // except for the number the user entered.
-        for (var i = 1; i <= 10; i++) {
-            if ((i % 2) == 0) {
+        for (var i = 1; i <= 50; i++) {
+            if (i % 2 === 0) {
                 continue;
-
-                console.log(i);
-
-                var oddNumber = prompt(oddNumber + "Yikes, not that one!")
-                console.log(oddNumber + "yikes, not that one!");
             }
-            oddNumber = prompt(oddNumber + " is an odd number")
+            if (b === oddNumber)
+                console.log(oddNumber + "yikes, not that one!");
+            continue;
+        }
+
             console.log(oddNumber + " is an odd number!");
         }
 
