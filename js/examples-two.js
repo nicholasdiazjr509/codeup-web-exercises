550b116f6c7760390a80c854a56dda4f8be6b209
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-23 16:11:39 -0500

    commit

--- js/examples-two.js
+++ js/examples-two.js
@@ -5,23 +5,44 @@
 //  var t= typeof e
 //  return"string"==t||"number"==t||"symbol"==t||"boolean"==t?"__proto__"!==e:null===e
     //}
-var weekday
-    var vacation;
-var boolean();
-    if (!weekday) {
-        console.log(weekday)
-        return true;
+// var weekday
+//     var vacation;
+//
+//     if (!weekday) {
+//         console.log(weekday)
+//         return true;
+//
+//     } else if (vacation) {
+//         console.log('boolean')
+//         return true;
+//
+//     }
+//     return false;
 
-    } else if (vacation) {
-        console.log('boolean')
-        return true;
 
+
+    function sleepIn(weekday, vacation){
+        return !weekday|| vacation;
+
+       console.log ();
     }
-    return false;
 
+    console.log(Boolean(''));
+    console.log(Boolean('0'));
+    console.log(Boolean(0));
 
+    console.log(Boolean(1));
+    console.log(Boolean([]));
+    console.log(Boolean(undefined));
 
+    console.log(!!undefined);
+    console.log();
 
+    var stringToBoolean = function(value) {
+        return value === 'true';
+    };
+    console.log(Boolean('false'));
+    console.log(stringToBoolean('false'));
 
 
 })();
