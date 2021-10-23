96ba61032aacbdebd12377d740776c7463dc17c5
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-23 16:47:04 -0500

    commit

--- js/examples-two.js
+++ js/examples-two.js
@@ -20,37 +20,39 @@
 //     return false;
 
 
-
+    {
     function sleepIn(weekday, vacation) {
-    if (!weekday || vacation)
-        return Boolean;
-    console.log(Boolean +'')
-    }
+        if (!weekday || vacation) {
+            return true;
+        } else {
+            return false;
+         //   console.log(sleepIn())
+        }
         //return !weekday || vacation;
         //console.log (Boolean('' ));
 
-       //console.log (Boolean( ));
+        //console.log (Boolean( ));
+    }
+            console.log('******************************************');
 
-console.log('******************************************');
+            console.log(Boolean()); // this is equal to false duh
 
-console.log(Boolean()); // this is equal to false duh
+            console.log(Boolean(''));
+            console.log(Boolean('0'));
+            console.log(Boolean(0));
 
-console.log(Boolean(''));
-    console.log(Boolean('0'));
-    console.log(Boolean(0));
+            console.log(Boolean(1));
+            console.log(Boolean([]));
+            console.log(Boolean(undefined));
 
-    console.log(Boolean(1));
-    console.log(Boolean([]));
-    console.log(Boolean(undefined));
+            console.log(!!undefined);
+            console.log();
 
-    console.log(!!undefined);
-    console.log();
+            var stringToBoolean = function (value) {
+                return value === 'true';
+            };
+            console.log(Boolean('false'));
+            console.log(stringToBoolean('false'));
+        }
 
-    var stringToBoolean = function(value) {
-        return value === 'true';
-    };
-    console.log(Boolean('false'));
-    console.log(stringToBoolean('false'));
-
-
-})();
+} ) ( ) ;
