ddcc7584c0f47ffb1634840d439c759fefcb0823
Nicholas <nicholasdiazjr509@gmail.com>
2021-10-30 08:55:21 -0500

    Trying to remember Java to do practice

--- js/solutions.js
+++ js/solutions.js
@@ -1,3 +1,60 @@
+/**if (a = true)  Then B, Else C, End if
+ * Java If-ElseIn this challenge, we test your knowledge of using if-else
+ * conditional statements to automate decision-making processes.
+ An if-else statement has the following logical flow:
+ Task
+ Given an integer, n, perform the following conditional actions:
+ If n  is odd, print  Weird
+ If n is even and in the inclusive range of 2 to 5, print  Not Weird
+ If n is even and in the inclusive range of 6 to 20, print  Weird
+ If n is even and greater than 20, print  Not Weird
+
+ Complete the stub code provided in your editor to print whether or not   is weird.
+ Input Format:
+ A single line containing a positive integer,n.
+ Constraints
+ 1 <= n <= 100
+
+ Output Format:
+ Print  Weird  if the number is weird; otherwise, print  Not Weird .
+ Sample Input 0
+ 3
+ Sample Output 0
+ Weird
+ Sample Input 1
+ Source: Wikipedia
+ 24
+ Sample Output 1
+ Not Weird
+
+ Explanation
+  Sample Case 0:
+ n is odd and odd numbers are weird, so we print  Weird .
+
+  Sample Case 1: n =24
+ n > 20 and n is even, so it isn't weird. Thus, we print  Not Weird  */
+
+
+
+if ( n !== 0) {
+    return weird;
+}
+/**Remember this?
+ *  Scanner sc=new Scanner(System.in);
+ for(int i=0; i<3; ++i) {
+         int a=sc.nextInt();
+      System.out.println(a);
+      }
+ }
+ }
+ */
+
+
+
+
+
+
+
 //1. A farmer is asking you to tell him how many legs can be counted among all his animals.
 //the farmer breeds three species:
 //The farmer has counted his animals, and he gives you a subtotal for each species.
