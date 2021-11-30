59eb1b824dc5abe66b603ada73675bbdd17fa060
Nicholas <nicholasdiazjr509@gmail.com>
2021-11-30 09:02:49 -0600

    ajax start today

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -5,7 +5,7 @@
         $(this).children().first().css("font-weight", "bold")
     });
 // $("li").last().html("Remember me!");
-//
+
 // $("#start-here").parent().parent().first().next().css("border", "solid 5px rebeccapurple");
 
 
@@ -38,7 +38,7 @@ $("dd").addClass("invisible");
 $("button").click(function(){
     $("dd").toggleClass("invisible")
 });
-$("btn-1").click(function() {
+$("#btn-1").click(function() {
         $("ul").each(function(){
             $(this).children().last().css({"background-color": "yellow", "width": "10%"})
     } )
@@ -46,10 +46,10 @@ $("btn-1").click(function() {
     $("dt").click(function(){
         $(this).css({ backgroundColor: "yellow", "width": "10%"})
 
-    });
-$("h3").addClass("highlighted").addClass("underline"); //affects all paragraphs// do not work retroactively  .add class etc.
-//
- $("highlighted").click(function (){
+
+// $("h3").addClass("highlighted").addClass("underline"); //affects all paragraphs// do not work retroactively  .add class etc.
+
+//  $("highlighted").click(function (){
 //     $(this).removeClass("highlighted");
 
 
