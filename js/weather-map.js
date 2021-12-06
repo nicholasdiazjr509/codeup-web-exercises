343dcdd26b86dcd343fe8c9e57d62cf43aa9f74a
Nicholas <nicholasdiazjr509@gmail.com>
2021-12-06 07:21:12 -0600

    more changes, weather not working

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,24 +1,8 @@
 "use strict";
 
 
-// $(document).ready(function() {
-    function popForecast(value) {
-        var date = moment.date(value.dt).format("dddd. MMM-DD-YYYY");
+ $(document).ready(function() {
 
-        return "<div id='every-box'  class='col-md-4'>"
-            + "<p id='date-time-group'>" + date + "</p>"
-            + "<p id='temp'>" + Math.round(value.temp.max) + "&#176;"
-            + Math.round(value.temp.min) + "#&176;" +  "< /p>"
-            + "<img src='https://openweathermap.org/img/w/"
-            + value.weather[0].icon + ".png'>"
-            + "<p id='conditions'>" + "<strong>Conditions:</strong> " //+ "</p>"
-            + value.weather[0].description + "</p>"
-            + "<p>" + "<strong>Winds:</strong> " + value.speed + "</p>"
-            + "<p>" + "<strong>Pressure:</strong> " + value.pressure + "</p>"
-            + "</div>";
-
-
-    }
 
     mapboxgl.accessToken = MAPBOX_KEY;
       const coordinates = document.getElementById('coordinates');
@@ -53,121 +37,137 @@
     marker.on('dragend', onDragEnd);
 
 
-// });
+                //found the api for the search
+                    // $("#search").click(function(){
+                    //     userInput = $("#user-input").val();
+                    //     moveMarkerToInput()
+                    // })
+    $("#mapStyle").change(function(){
+        map.setStyle($("#mapStyle").val())
+    })
+$(document).ready(() => {
+    function write() {
+        $.ajax("https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + coordinates[0] + "&lon="
+            + coordinates[1] + "&exclude=minutely,hourly&appid=" + OPEN_WEATHER_KEY)
+            .done(function (response) {
+                $("#insertInfo").html(" ");
+                for (var i = 0; i < response.daily.length; i++) {
+                    var today = response.daily[i];
+                    var todaysDate = new Date(today.dt * 1000);
+                    todaysDate = todaysDate.toDateString()
+                    var html = ""
+                    html = "<div class='container grow col-sm weather-" + (i + 1) + "'>";
+                    html += "<div class='headerContainer'><h3>" + todaysDate + "</h4></div>"
 
-function write(){
-    $.ajax("https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + coordinates[0] + "&lon="
-    + coordinates[1] + "&exclude=minutely,hourly&appid=" + OPEN_WEATHER_KEY)
-        .done(function(response){
-            $("#insertInfo").html("");
-    for (var i = 0; i < response.daily.length; i++){
-        var today = response.daily[i];
-        var todaysDate = new Date(today.dt * 1000);
-        todaysDate = todaysDate.toDateString()
-        var html = ""
-            html = "<div class='container grow col-sm weather-" + (i + 1) + "'>";
-            html += "<div class='headerContainer'><h3>" + todaysDate + "</h4></div>"
+                    //current weather
+                    if (i === 0) {
+                        html += "<h1 style='background-image: url(https://openweathermap.org/img/w/" +
+                            todays.weather[0].icon + ".png);background-repeat:no-repeat; background-position: top;'>" +
+                            Math.round(response.current.temp) + "°</h1>";
+                    } else {
+                        html += "<h1 style='background-image: url(https://openweathermap.org/img/w/" +
+                            todays.weather[0].icon + ".png);background-repeat: no-repeat; background-position: top;'>" +
+                            Math.round(todays.temp.day) + "°</h1>";    //HexCode???	"\u00B0" degree sign
+                    }
 
-                //current weather
-                if( i === 0)
-        {
-            html += "<h1 style='background-image: url(https://openweathermap.org/img/w/" +
-                todays.weather[0].icon + ".png);" +
-                "background-repeat: no-repeat; background-position: top;'>" +
-                Math.round(todays.temp.day) + "\u00B0 </h1>";
-        }else {
-                    html += "<h1 style='background-image: url(https://openweathermap.org/img/w/" +
-                        todays.weather[0].icon + ".png);" +
-                        "background-repeat: no-repeat; background-position: top;'>" +
-                        Math.round(todays.temp.day) + "\u00B0 </h1>"; //HexCode	"\u00B0" degree sign
+                    html += "<p> Real Feel: " + Math.round(today.feels - like.day) + "\u00B0°</p>";   // / ° /
+                    html += "<div>low: " + Math.round(today.temp.min) + "° / High: " + Math.round(today.temp.max) + "°</div>";
+                    html += "<div>Humidity: " + today.humidity + "%</div>";
+                    html += "<div class='onBottom'>Chance of rain: " + Math.round(today.pop * 100) + "%</div";
+                    html += "<div class='hideTillClick hidden'> ";
+
+                    html += "<div style='border-bottom: 2px solid black; width: 100%;'><p style='margin: 0 35%;'>Wind</p></div>"
+                    html += "<div style='text-align: center'>";
+                    html += "<div>" + windDirection(today.wind_deg) + " " + Math.round(today.wind_speed) + "MPH</div>";
+                    html += "<div>Gusts: " + Math.round(today.wind_gust) + "MPH</div></div>";
+
+                    html += "<div class='riseSet'><i class=\"fas fa-sun\"></i></div>";
+                    html += "<div class='row'>";
+                    html += "<div class='col'>Rise:<p>" + riseSet(today.sunrise) + "am</p></div>";
+                    html += "<div class='col'>Set:<p>" + riseSet(today.sunset) + "pm</p></div></div>";
+
+                    html += "<div class='riseSet'><i id='moon' class=\"fas fa-moon\"></i></div>";
+                    html += "<div class='row'>";
+                    html += "<div class='col'>Rise: <p>" + riseSet(today.moonset) + "pm</p></div>";
+                    html += "<div class='col'>Set:<p>" + riseSet(today.moonset) + "am</p></div></div>";
+                    html += "</div>";
+                    $("#insertInfo").append(html);
+
+
+                    $(".weather-" + (i + 1) + "").click(function () {
+                        $(".hideTillClick").slideToggle(1000);
+                    })
+                    $(".weather-5, .weather-6, .weather-7, .weather-8").addClass("hideTill");
                 }
-        $(".weather-" + (i + 1) + "").click(function(){
-            $(".hideTillClick").slideToggle(1000);
-               })
-        $(".weather-5, .weather-6, .weather-7, .weather-8").addClass("hideTill");
+            });
     }
-       });
-            }write();
+
+    write();
 
 
+    $("#toggleCards").click(toggle);//used for card enable-disable
 
+    function windDirection(windDirection) {
+        var cardinalDirection = "";
+        //used 360 degree direction
+        if (windDirection >= 349 || windDirection <= 11) {
+            cardinalDirection = "N";
+        } else if
+        (windDirection >= 12 && windDirection <= 34) {
+            cardinalDirection = "NNE"
+        } else if
+        (windDirection >= 35 && windDirection <= 56) {
+            cardinalDirection = "NE";
+        } else if
+        (windDirection >= 57 && windDirection <= 79) {
+            cardinalDirection = "ENE";
+        } else if
+        (windDirection >= 80 && windDirection <= 101) {
+            cardinalDirection = "NNE"
+        } else if
+        (windDirection >= 102 && windDirection <= 124) {
+            cardinalDirection = "ESE";
+        } else if
+        (windDirection >= 125 && windDirection <= 146) {
+            cardinalDirection = "SSE";
+        } else if
+        (windDirection >= 147 && windDirection <= 169) {
+            cardinalDirection = "NNE"
+        } else if
+        (windDirection >= 170 && windDirection <= 191) {
+            cardinalDirection = "ESE";
+        } else if
+        (windDirection >= 192 && windDirection <= 214) {
+            cardinalDirection = "SSW";
+        } else if
+        (windDirection >= 215 && windDirection <= 236) {
+            cardinalDirection = "SW"
+        } else if
+        (windDirection >= 237 && windDirection <= 258) {
+            cardinalDirection = "WSW";
+        } else if
+        (windDirection >= 259 && windDirection <= 281) {
+            cardinalDirection = "W";
+        } else if
+        (windDirection >= 282 && windDirection <= 303) {
+            cardinalDirection = "WNW"
+        } else if
+        (windDirection >= 304 && windDirection <= 326) {
+            cardinalDirection = "NW";
+        } else if
+        (windDirection >= 327 && windDirection <= 348) {
+            cardinalDirection = "NNW";
+        } else {
+            cardinalDirection = "ERROR";
+        }
+        return windDirection;
+    }
 
+    function riseSet(item) {//sun/moon rise and set
+        var readableItem = new Date(item * 1000);
+        readableItem = readableItem.getHours() + ":" + readableItem.getMinutes();
+        return readableItem;
+    }
 
-
-    // weatherSA.done(function (data) {
-    //     // console.log(data)
-    //     $("#sub-header").text(data.city.name);
-    //     //attr("src", "https://openweathermap.org/img/w/"+ data.daily[0].weather[0].icon + ".png");
-    //     //console.log(data.daily[0].dt);      //it's times 1000   //for conversion of UTC date  (unviversal time conversion?)
-    //     //console.log(new Date(data.daily[0].dt * 1000));
-    //     var userWeatherInfo = '';
-    //     data.list.forEach(function (value) {
-    //         userWeatherInfo += popForecast(value);
-    //         $("#cover-div").html(userWeatherInfo);
-    //
-    //    var myLatLng = new google.maps.LatLng(29.426549, -98.490018);
-    //     var mapOptions = {
-    //         center: myLatLng
-    //     };
-    //
-    //     $.get("https://api.openweathermap.org/data/2.5/onecall", {   //var userPick =
-    //         lat: 29.423017,
-    //         lon: -95.48527,
-    //         APPID: OPEN_WEATHER_KEY,
-    //         units: "imperial"
-    //     });
-
-    // map.setCenter(marker.position);
-    // marker.addTo(map);
-//**autocomplete?? DO NOT NEED. Used MapBox documentation find how to use GEOCODE and for search box for user.
-//     var autocomplete = new google.maps.places.Autocomplete(
-//         (document.getElementById("location")),
-//         {types: ["geocode"]});
-//
-
-
-//
-//
-// });
-//var openMenu = false;
-// var navigation = document.getElementById("nav-website");
-// var userDropdown = document.getElementById('user-dropdown-menu');
-// var supportDropdown = document.getElementById('support-dropdown-menu');
-//
-// function unfoldMenu() {
-//   navigation.classList.add('visible');
-// }
-//
-// function foldMenu() {
-//   navigation.classList.remove('visible')
-// }
-//
-// function toggleDropdown(el) {
-//   el.classList.contains('dropdown-visible') ? el.classList.remove('dropdown-visible') : el.classList.add('dropdown-visible')
-// }
-//
-// function closePopUps() {
-//     userDropdown && userDropdown.classList.remove('dropdown-visible')
-//     supportDropdown.classList.remove('dropdown-visible')
-// }
-//
-// function toggleMenu (e) {
-//     e.stopImmediatePropagation();
-//     if (e.target) {
-//       if (e.target.id === 'hamburger' || e.target.parentNode.id === 'hamburger') {
-//         openMenu = !openMenu;
-//         openMenu ? unfoldMenu() : foldMenu();
-//       } else if (!e.target.nodeName === 'input') {
-//         openMenu = false;
-//         foldMenu();
-//       } else if (e.target.id === 'user-dropdown' || e.target.parentNode.id === 'user-dropdown') {
-//         supportDropdown.classList.remove('dropdown-visible')
-//         toggleDropdown(userDropdown)
-//       } else if (e.target.id === 'support-dropdown') {
-//         userDropdown && userDropdown.classList.remove('dropdown-visible')
-//         toggleDropdown(supportDropdown)
-//       } else {
-//         closePopUps()
-//       }
-//     }
-//   }
+ })();
+ })
