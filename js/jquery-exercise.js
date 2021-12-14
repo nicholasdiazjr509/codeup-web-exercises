096421516623f804810fef376277d463326e0390
Nicholas <nicholasdiazjr509@gmail.com>
2021-12-14 05:22:44 -0600

    somehow I changed branches mid exercise?

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,11 +1,56 @@
-"use strict"
+// $(document).ready(function(){
+// var output = $("#heading").html();
+
+$("ul").each(function(){
+    $(this).children().first().css("font-weight", "bold")
+});
+// $("li").last().html("Remember me!");
+//
+// $("#start-here").parent().parent().first().next().css("border", "solid 5px rebeccapurple");
 
 
-$(document).ready(function(){
+$("#state-parks-toggle").click(function(){
+    $('#state-parks-texas').slideToggle();
+})
 
+$('#national-parks-toggle').click(function(){
+    $('#national-parks').fadeToggle("slow", "linear");
+});
+$("dt").each(function() {
+    $("dt").on("click", function () {
+        $(this).next().slideToggle();
+    })
+});
 
+$("li").click(function(){
+    $("ul").each(function(){
+        $(this).children().first().css("color", "blue")
 
+    })
+});
+$("h3").click(function(){
+    $("ul").each(function(){
+        $(this).children().css("font-weight", "bold")
+    })
+});
+$("dd").addClass("invisible");
 
+$("button").click(function(){
+    $("dd").toggleClass("invisible")
+});
+$("btn-1").click(function() {
+    $("ul").each(function(){
+        $(this).children().last().css({"background-color": "yellow", "width": "10%"})
+    } )
+});
+$("dt").click(function(){
+    $(this).css({ backgroundColor: "yellow", "width": "10%"})
+
+});
+$("h3").addClass("highlighted").addClass("underline"); //affects all paragraphs// do not work retroactively  .add class etc.
+//
+$("highlighted").click(function (){
+//     $(this).removeClass("highlighted");
 
 
 });
---
+++ js/keys.js
@@ -0,0 +1,11 @@
+const MAPBOX_KEY = "pk.eyJ1IjoiaGVsbHRyb29wZXI1MDkiLCJhIjoiY2t3cGMyOTFyMGJtMTJ1bzRlZTdhbHpvcSJ9.16k-dtMJzRE-uaVdYvxHcQ";
+// const TOKEN = "pk.eyJ1IjoiaGVsbHRyb29wZXI1MDkiLCJhIjoiY2t3cGMyOTFyMGJtMTJ1bzRlZTdhbHpvcSJ9.16k-dtMJzRE-uaVdYvxHcQ";
+const OPEN_WEATHER_KEY = "ac7749b330eeb32bea5115c8400b2da2";
+const GITHUB_KEY = "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";
+const MOVIE_API_KEY= "4e5a73d1dd5eaaafd1e32a1411a6bd53";
+
+//<div id="openweathermap-widget-11"></div>
+// <script src='//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js'>
+// </script><script>window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
+// window.myWidgetParam.push({id: 11,cityid: '2643743',appid: 'ac7749b330eeb32bea5115c8400b2da2',units:
+// 'metric',containerid: 'openweathermap-widget-11',  });  (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();</script>
