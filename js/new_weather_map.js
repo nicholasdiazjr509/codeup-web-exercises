7e0a700a76be4ad14ae0699dc0f52163c8e3cf0a
Nicholas Diaz Jr <nicholasdiazjr509@gmail.com>
2022-04-08 16:22:57 -0500

    getting done with weather map

--- js/new_weather_map.js
+++ js/new_weather_map.js
@@ -1,10 +1,10 @@
 const timeEl = document.getElementById('time');
-const datEl = document.getElementById('date');
+const dateEl = document.getElementById('date');
 const currentWeatherItemsEl = document.getElementById('current-weather-items');
 const timezone = document.getElementById('timezone');
 const countEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
-const currentTemEl = document.getElementById('current-temp');
+const currentTempEl = document.getElementById('current-temp');
 
 // now update all of the data
 
@@ -20,14 +20,14 @@ setInterval(() => {
     const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
-    const hours = time.getHours();
+    const hour = time.getHours();
     const hoursIn12HrFormat = hour >= 13 ? hour %12: hour;
     const minutes = time.getMinutes();
-    const ampm = hours >=12 ? 'PM' : 'AM'
+    const ampm = hour >=12 ? 'PM' : 'AM'
 
     timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
 
-    DataTransferItemList.innerHTML = days[day] + ', ' + date + ' ' + months[month]
+    dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 }, 1000);
 
 
@@ -35,5 +35,40 @@ getWeatherData()
 function getWeatherData (){
     navigator.geolocation.getCurrentPosition((success) => {
         console.log(success);
+   let {latitude, longitude} = success.coords;     
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${OPEN_WEATHER_KEY}`)
+        .then(response => response.json()).then(data => {
+            console.log(data)
+            showWeatherData(data);
+        })
     })
+}
+function showWeatherData(data){
+    let{humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+    timezone.innerHTML = data.timezone;
+    countryEl.innerHTML = data.lat + 'N ' + data.lon + 'E'
+
+    currentWeatherItemsEl.innerHTML=
+`       <div class="weather-item">
+            <div>Humidity</div>
+            <div>${humidity}</div>
+        </div>
+        <div class="weather-item">
+                <div>Pressure</div>
+                <div>${pressure}</div>
+        </div>
+        <div class="weather-item">
+                <div>Wind Speed</div>
+                <div>${wind_speed}</div>
+        </div>
+        <div class="weather-item">
+                <div>Sunset</div>
+                <div>${window.moment(sunset * 1000).format('HH:mm a')}</div>
+        </div>
+        <div class="weather-item">
+        <   div>Sunrise</div>
+            <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+        </div>
+        `;
 }
