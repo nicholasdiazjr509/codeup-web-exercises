0976152b4c004dcf8535b9a8ad91290179532d42
Nicholas Diaz Jr <nicholasdiazjr509@gmail.com>
2022-04-18 15:42:51 -0500

    created buble sort

---
+++ java-folder/Bubble.java
@@ -0,0 +1,23 @@
+class Bubble{
+    public static void bubbleSort(int a[]){
+        for(int i=0; i < a.length; i++){
+            for(int j=0; j < a.length-i-1; j++){
+                if(a[j] > a[j + 1]){
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int a[] = {4, 6, 1, 3, 8, 10, 9, 5, 2};
+        bubbleSort(a);
+
+        //print the array
+        for(int i=0; i < 10; i++){
+            System.out.println(a[i] + " ");
+        }
+        System.out.println("");
+    }
+}
