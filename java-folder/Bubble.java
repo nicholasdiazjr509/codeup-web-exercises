33faf76de9844580d25d2d0dc1ab6f22fcd5701e
Nicholas Diaz Jr <nicholasdiazjr509@gmail.com>
2022-04-18 15:50:43 -0500

    optimized bubble sort

--- java-folder/Bubble.java
+++ java-folder/Bubble.java
@@ -1,13 +1,17 @@
 class Bubble{
     public static void bubbleSort(int a[]){
         for(int i=0; i < a.length; i++){
+            boolean swapped = false;
             for(int j=0; j < a.length-i-1; j++){
                 if(a[j] > a[j + 1]){
                     int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
+                    swapped = true;
                 }
             }
+            if(!swapped)
+            break;
         }
     }
     public static void main(String[] args) {
@@ -18,6 +22,6 @@ class Bubble{
         for(int i=0; i < 10; i++){
             System.out.println(a[i] + " ");
         }
-        System.out.println("");
+       System.out.println("");
     }
 }
