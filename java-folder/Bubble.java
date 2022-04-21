3ef1e84eaca5da687fae740d64318cdfd5fe238a
Nicholas Diaz Jr <nicholasdiazjr509@gmail.com>
2022-04-20 19:26:39 -0500

    creating portfolio site

--- java-folder/Bubble.java
+++ java-folder/Bubble.java
@@ -25,3 +25,15 @@ class Bubble{
        System.out.println("");
     }
 }
+
+/* Optimized Version of Bubble Sort
+
+To check if the array is sorted or not, we can check if any element is getting swapped in the iteration or not. 
+If none of the elements are getting swapped, then it means that the array is sorted and we can stop the process of Bubble Sort. 
+So, let's use a variable to keep the record of swapping and modify the previous code according to it. 
+
+Here, we are using an extra variable 'swapped' to store whether we are swapping in the middle of the algorithm or not. 
+We are setting the 'swapped' value to true after swapping something - swapped = TRUE and at last,
+we are checking if something was swapped or not in the process - if not(swapped), 
+then we are breaking the loop if nothing was swapped and hence meaning that the loop is already sorted.
+*/
